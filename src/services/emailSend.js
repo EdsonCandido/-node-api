16f02b4063c7481a82b9df3d2cfc820a0617eab6
Edson Cândido Alves Júnior <edson.c.a.j@hotmail.com>
2019-04-10 10:55:11 -0300

    Update emailSend.js

--- src/services/emailSend.js
+++ src/services/emailSend.js
@@ -7,15 +7,15 @@ exports.send = async (emailUser, token) => {
  let transporte = nodemailer.createTransport({
     service: 'Gmail', // Como mencionei, vamos usar o Gmail
     auth: {
-      user: 'edsonj85@gmail.com', // Basta dizer qual o nosso usuário
-      pass: 'nosde123!'             // e a senha da nossa conta
+      user: 'seu email', // Basta dizer qual o nosso usuário
+      pass: 'senha '             // e a senha da nossa conta
     } 
   });
 
   let email = {
-    from: 'edsonj85@gmail.com', // Quem enviou este e-mail
+    from: 'PDe quem', // Quem enviou este e-mail
     to: emailUser, // Quem receberá
-    subject: 'Recupetar senha {{Sitema}}',
+    subject: 'Assunto {{Sitema}}',
     html: token // O conteúdo do e-mail
   };
   
