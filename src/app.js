546f7b4b0d8a5593ca3f1f5d7c03b579bda8d947
EdsonCandido <edson.c.a.j@hotmail.com>
2019-04-10 10:44:32 -0300

    Raiz

--- src/app.js
+++ src/app.js
@@ -16,12 +16,25 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useCreateIndex: 
  */
 const indexRoutes = require('./routes/indexRouter');
 const productRoutes = require('./routes/productRouter');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const projectRoutes = require('./routes/projectRoute');
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.json());
 app.use(morgan('dev'));
-
+//HABILITA O CORS
+app.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', indexRoutes);
 app.use('/products', productRoutes);
+app.use('/user', userRoutes);
+app.use('/orders', orderRoutes);
+app.use('/project', projectRoutes);
 
 module.exports = app;
